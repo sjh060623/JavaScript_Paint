f0b22dab6bbfc782ce5bb881b159167cc5d3a9ad
손정훈 <sjh060623@gmail.com>
2022-04-02 23:31:28 +0900

    JavaScript_Paint 2.0

--- app.js
+++ app.js
@@ -1,1 +1,27 @@
-console.log("hi");
+const canvas = document.getElementById("jsCanvas");
+
+let painting = false;
+
+function stopPainting() {
+  painting = false;
+}
+
+function onMouseMove(e) {
+  const x = e.offsetX;
+  const y = e.offsetY;
+}
+
+function onMouseDown(e) {
+  painting = true;
+}
+
+function onMouseUp(e) {
+  stopPainting();
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", onMouseDown);
+  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
