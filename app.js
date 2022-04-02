5d48d894a84b154d84e77a21eb996ee8cd8a0e6e
손정훈 <sjh060623@gmail.com>
2022-04-02 23:45:31 +0900

    JavaScript_Paint 2.1

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
+function startPainting() {
+  painting = true;
+}
 
 function stopPainting() {
   painting = false;
@@ -9,19 +17,22 @@ function stopPainting() {
 function onMouseMove(e) {
   const x = e.offsetX;
   const y = e.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
 function onMouseDown(e) {
   painting = true;
 }
 
-function onMouseUp(e) {
-  stopPainting();
-}
-
 if (canvas) {
   canvas.addEventListener("mousemove", onMouseMove);
-  canvas.addEventListener("mousedown", onMouseDown);
-  canvas.addEventListener("mouseup", onMouseUp);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
   canvas.addEventListener("mouseleave", stopPainting);
 }
